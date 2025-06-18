44dc12ff4bfd7b9cbf8cdc58c4a3bcd682816055
Bimo Ade <bimoadebudimanfikri@gmail.com>
2025-06-18 13:23:51 +0700

    Simplified workflow: Removed DagsHub remote tracking, focus on local mlruns for Docker/Git artifact.

--- MLproject/modelling.py
+++ MLproject/modelling.py
@@ -10,9 +10,7 @@ import os
 import argparse
 import matplotlib.pyplot as plt
 import seaborn as sns
-import dagshub
 
-dagshub.init(repo_owner='kkokonatsu', repo_name='Workflow-CI', mlflow=True)
 
 LE_CLASSES = ['Extrovert', 'Introvert'] 
 
